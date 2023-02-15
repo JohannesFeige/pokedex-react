cca48cc90418b00c00ac74fde1539c660a6cb7a7
JohannesFeige <johannesfeige@gmail.com>
2023-02-15 16:08:38 +0100

    remove loadingContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import styles from './App.module.scss'
 import { useAppSelector } from './appStore'
 import { LoadingSpinner } from './components/LoadingSpinner'
-import { useLoading } from './context/loadingContext'
 import { Header } from './features/Header'
 import { Pokedex } from './features/Pokedex'
 import { PokemonDetail } from './features/PokemonDetail/PokemonDetail'
@@ -24,8 +23,6 @@ const router = createBrowserRouter(
 )
 
 export const App: React.FC = () => {
-    const { isLoading } = useLoading()
-
     const queryPending = useAppSelector((state) => Object.values(state.api.queries).some((query) => query?.status === 'pending'))
 
     return (
@@ -34,7 +31,7 @@ export const App: React.FC = () => {
                 <Header />
                 <RouterProvider router={router} />
             </div>
-            <LoadingSpinner show={isLoading || queryPending} />
+            <LoadingSpinner show={queryPending} />
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { App } from './App'
 import { appStore } from './appStore'
-import { LoadingProvider } from './context/loadingContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <LoadingProvider>
-            <Provider store={appStore}>
-                <App />
-            </Provider>
-        </LoadingProvider>
+        <Provider store={appStore}>
+            <App />
+        </Provider>
     </React.StrictMode>
 )
